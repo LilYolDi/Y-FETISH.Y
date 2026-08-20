d0c1ad6844a7018228a89e0937623e4372d09434
LilYolDi <107432353+LilYolDi@users.noreply.github.com>
2026-08-20 19:35:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -161,3 +161,14 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Y-New запущен на порту ${PORT}`);
 });
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Y-New запущен на порту ${PORT}`);
+});
